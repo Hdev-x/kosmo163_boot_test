6a7ea3b88cc6748a4264e88f6fad520cdadb14fb
semingit <semin0216@naver.com>
2026-04-02 17:42:48 +0900

    bootstrap 기본적인 요소

--- src/main/java/com/semin/app/home/HomeController.java
+++ src/main/java/com/semin/app/home/HomeController.java
@@ -9,6 +9,6 @@ public class HomeController {
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home() throws Exception {
-		return "/WEB-INF/views/index.jsp";
+		return "index";
 	}
 }
